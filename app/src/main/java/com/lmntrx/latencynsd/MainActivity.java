5178cb3a2bea2eded40f3da88ec412bfbc45b99e
Aswin CJ <aswincj4@gmail.com>
2016-11-09 08:03:34 +0530

    almost worked

--- app/src/main/java/com/lmntrx/latencynsd/MainActivity.java
+++ app/src/main/java/com/lmntrx/latencynsd/MainActivity.java
@@ -10,12 +10,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ExecutionException;
@@ -26,6 +34,12 @@ public class MainActivity extends AppCompatActivity {
     private String SERVICE_NAME = "NSD";
     private String SERVICE_TYPE = "_http._tcp.";
     private NsdManager mNsdManager;
+    ServerSocket serverSocket;
+
+    int discoveredPORT;
+    InetAddress discoveredHost;
+
+    InetAddress handshakeIP;
 
     boolean serviceHosting = false;
     boolean serviceDiscovering = false;
@@ -35,6 +49,21 @@ public class MainActivity extends AppCompatActivity {
     Handler handler;
     TextView display;
     String ans;
+
+    int okToContinueClient=0;
+    int okToDisplayHandshake=0;
+    int okToContinueHost=0;
+
+    Timer timer2;
+
+
+
+    Handler loadingHandler;
+    Runnable loadingRunnable;
+    int loadingcount;
+
+
+
 
 
     private static final String TAG = "MainActivity";
@@ -50,7 +79,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       final Button hostBtn= (Button) findViewById(R.id.hostBTN);
+        final  Button discoverBtn= (Button) findViewById(R.id.discoverBTN);
+        final LinearLayout buttonsLL = (LinearLayout) findViewById(R.id.ButtonsLinear);
+        final TextView handshakeTV= (TextView) findViewById(R.id.handshakeTxtView);
+        final  TextView handshakeDots= (TextView) findViewById(R.id.handshakeDots);
+        final TextView header= (TextView) findViewById(R.id.header);
 
+
+//    try
+//    {
+//        initializeServerSocket();
+//    }
+//    catch (IOException e)
+//    {
+//
+//    }
 
 
 
@@ -58,9 +102,8 @@ public class MainActivity extends AppCompatActivity {
 
         mNsdManager = (NsdManager) getSystemService(Context.NSD_SERVICE);
 
-        Button hostBtn= (Button) findViewById(R.id.hostBTN);
-        Button discoverBtn= (Button) findViewById(R.id.discoverBTN);
-        Button currentTimeBtn = (Button) findViewById(R.id.currentTimeButton);
+
+
 
     //--------------------------------TIMER FUNCTIONS---------------------------------------------------------
 
@@ -83,14 +126,14 @@ public class MainActivity extends AppCompatActivity {
         handler = new Handler();
 
         final int[] seconds = {0};
-        display = (TextView) findViewById(R.id.TimeTV);
+        //display = (TextView) findViewById(R.id.TimeTV);
         updater = new Runnable() {
             @Override
             public void run() {
                 seconds[0] += 500;
                 currentTime=serverTime+seconds[0];
                 finalTimer=currentTime-serverTime;
-                display.setText("t: " + finalTimer);
+                //display.setText("t: " + finalTimer);
 
             }
         };
@@ -104,18 +147,18 @@ public class MainActivity extends AppCompatActivity {
         Timer timer = new Timer();
         timer.schedule(timerTask,500,500);
 
-        TextView sTime = (TextView) findViewById(R.id.serverTime);
-        final TextView cTime = (TextView) findViewById(R.id.currentTime);
+        //TextView sTime = (TextView) findViewById(R.id.serverTime);
+        //final TextView cTime = (TextView) findViewById(R.id.currentTime);
 
-        sTime.setText("Server Time: "+serverTime);
-
-        currentTimeBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-
-                cTime.setText("Current Timer:"+finalTimer);
-
-            }
-        });
+//        sTime.setText("Server Time: "+serverTime);
+//
+//        currentTimeBtn.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//
+//                cTime.setText("Current Timer:"+finalTimer);
+//
+//            }
+//        });
 
 
 
@@ -126,15 +169,212 @@ public class MainActivity extends AppCompatActivity {
 
         hostBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                try {
 
-                    serviceDiscovering=false;
-                    serviceHosting=true;
-                    initializeServerSocket();
-                    registerService(SocketServerPORT);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+
+                // EDAAAA  EEE GONE appo thanne work aavilla. HANDSHAKE KAZHINJITTE UI UPDATE AAVANOLLU :/  IVIDE ENTHU GONE/VISIBLE AAKIYALUM
+//
+//                Log.d(TAG, "REQUIRED TRY BLOCK 1");
+//
+//
+//                Socket socket = serverSocket.accept();
+//                // loadingTimer.cancel();
+//
+//
+//
+//                Log.d(TAG, "REQUIRED TRY BLOCK 2");
+
+
+  //                thaazhe kidakkane ee code execute aayitte ee visibility update aavu :/  KOPP  EVERYTHING WORKS IN LOG
+                //
+                buttonsLL.setVisibility(View.GONE);
+
+
+
+
+
+
+
+
+
+
+
+             //   try {
+//
+//
+//
+//
+//
+//
+//
+//                    loadingHandler = new Handler();
+//
+//                    loadingcount=1;
+//
+//                    loadingRunnable = new Runnable() {
+//                        @Override
+//                        public void run() {
+//
+//                            switch (loadingcount)
+//                            {
+//                                case 1: handshakeDots.setText(".."); loadingcount=2;
+//                                    break;
+//                                case 2: handshakeDots.setText("..."); loadingcount=3;
+//                                    break;
+//                                case 3: handshakeDots.setText("...."); loadingcount=4;
+//                                    break;
+//                                case 4: handshakeDots.setText("."); loadingcount=1;
+//                                    break;
+//                            }
+//
+//                        }
+//                    };
+//
+//                    TimerTask loadingtimerTask = new TimerTask() {
+//                        @Override
+//                        public void run() {
+//                            loadingHandler.post(loadingRunnable);
+//                        }
+//                    };
+//                    final Timer loadingTimer = new Timer();
+//                    loadingTimer.schedule(loadingtimerTask,500,500);
+//
+//
+
+
+
+
+
+
+                    serviceDiscovering = false;
+                    serviceHosting = true;
+
+
+
+
+
+                    Log.d(TAG, "REGISTERED PORT 1= " + SocketServerPORT);
+
+                    //----WHILE
+
+//
+//                        timer2 = new Timer();
+//                        timer2.scheduleAtFixedRate(new TimerTask() {
+//                            @Override
+//                            public void run() {
+//
+
+
+
+                               Thread handshake= new Thread() {
+
+                                    @Override
+                                    public void run() {
+                                        //------------------------------
+
+
+                                        try {
+
+                                            initializeServerSocket();
+                                            registerService(SocketServerPORT);
+                                            header.setText("Hosted on Port:"+SocketServerPORT);
+
+
+
+
+                                            Log.d(TAG, "REQUIRED TRY BLOCK 1");
+
+
+                                            Socket socket = serverSocket.accept();
+                                           // loadingTimer.cancel();
+
+
+
+                                            Log.d(TAG, "REQUIRED TRY BLOCK 2");
+
+
+                                            DataInputStream dataInputStream = new DataInputStream(
+                                                    socket.getInputStream());
+
+                                            Log.d(TAG, "REQUIRED TRY BLOCK 3");
+                                            DataOutputStream dataOutputStream = new DataOutputStream(
+                                                    socket.getOutputStream());
+
+                                            Log.d(TAG, "REQUIRED TRY BLOCK 4");
+
+
+                                            String messageFromClient;
+
+                                            Log.d(TAG,"BEFORE READING FROM INPUT STREAM");
+
+                                            //If no message sent from client, this code will block the program
+                                            messageFromClient = dataInputStream.readUTF();
+                                            Log.d(TAG,"AFTER READING FROM INPUT STREAM");
+
+
+                                            Log.d(TAG,"messagefromClient:: "+messageFromClient);
+
+
+
+
+
+                                                Log.d(TAG, "MESSAGE::::" + messageFromClient);
+
+                                            handshakeIP=InetAddress.getByName(messageFromClient);
+
+
+
+
+
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+
+
+                                        //-------------------------
+                                    }
+                                };
+
+
+
+                                handshake.start();
+
+                                try
+                                {
+                                    handshake.join();
+
+                                }catch (InterruptedException e)
+                                {
+
+                                }
+
+
+
+//                                TextView debug= (TextView) findViewById(R.id.debug);
+//
+//                                debug.setText("Handshake IP:"+handshakeIP);
+
+
+
+
+
+
+
+
+
+//                            }
+//                        }, 1000, 1000);
+
+
+
+
+
+
+
+
+                    //-------------------WHILE ENDED ^^
+
+
+
 
             }
         });
@@ -143,11 +383,69 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Log.d("Entered Onclick","Entered Onclick");
 
+                buttonsLL.setVisibility(View.GONE);
+
                 serviceHosting=false;
                 serviceDiscovering=true;
                 initializeDiscoveryListener();
-                mNsdManager.discoverServices(
-                        SERVICE_TYPE, NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
+                mNsdManager.discoverServices(SERVICE_TYPE, NsdManager.PROTOCOL_DNS_SD, mDiscoveryListener);
+
+                Log.d(TAG, "DISCOVERED PORT 1= " + discoveredPORT);
+
+                while(okToContinueClient==0)
+                {
+
+                    Timer timer;
+                    timer = new Timer();
+                    timer.scheduleAtFixedRate(new TimerTask() {
+                        @Override
+                        public void run() {
+                            //Generate number
+                        }
+                    }, 2000, 2000);
+
+                    if (discoveredPORT!=0)
+                    {
+                        okToContinueClient=1;
+                        timer.cancel();
+                    }
+                }
+
+                new Thread(){
+
+                    @Override
+                    public void run(){
+
+                        try {
+
+
+
+                    Socket socket = new Socket(discoveredHost,discoveredPORT);
+                    DataInputStream dataInputStream = new DataInputStream(
+                            socket.getInputStream());
+                    DataOutputStream dataOutputStream = new DataOutputStream(
+                            socket.getOutputStream());
+
+                    String msgToServer=getIpAddress();
+
+                    dataOutputStream.writeUTF(msgToServer);
+                            Log.d(TAG,"Did it work?");
+
+                    //TextView debug= (TextView) findViewById(R.id.debug);
+                    //debug.setText("Message Wrote:"+msgToServer);
+                }
+                catch (IOException e)
+                {
+
+                }
+
+                        }
+                }.start();
+
+
+
+//
+
 
 
             }
@@ -236,10 +534,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void initializeServerSocket() throws IOException {
         // Initialize a server socket on the next available port.
-        ServerSocket serverSocket = new ServerSocket(0);
+        serverSocket = new ServerSocket(0);
 
         // Store the chosen port.
         SocketServerPORT =  serverSocket.getLocalPort();
+
 
     }
 
@@ -250,8 +549,11 @@ public class MainActivity extends AppCompatActivity {
             public void onServiceRegistered(NsdServiceInfo NsdServiceInfo) {
                 String mServiceName = NsdServiceInfo.getServiceName();
                 SERVICE_NAME = mServiceName;
-                Toast.makeText(MainActivity.this, "Successfully registered",
+                Toast.makeText(MainActivity.this, "Hosted Successfully.WAITING FOR OTHER DEVICE...",
                         Toast.LENGTH_LONG).show();
+
+
+
                 Log.d("NsdserviceOnRegister", "Registered name : " + mServiceName);
             }
 
@@ -309,6 +611,7 @@ public class MainActivity extends AppCompatActivity {
 
                     initializeResolveListener();
                     mNsdManager.resolveService(service, mResolveListener);
+
 
                 }
 
@@ -368,17 +671,60 @@ public class MainActivity extends AppCompatActivity {
                 int port = mService.getPort();
                 InetAddress host = mService.getHost();
 
+                discoveredPORT=port;
+                discoveredHost=host;
+
+
+                Log.d(TAG, "DISCOVERED PORT 2= " + port);
+                Log.d(TAG, "HOST ADDRESS= " + host);
+
+
                 Toast.makeText(MainActivity.this, "Successfully connected",
                         Toast.LENGTH_LONG).show();
 
+
+
+
+
                 if (serviceInfo.getServiceName().equals(SERVICE_NAME)) {
                     Log.d(TAG, "Same IP.");
-                    return;
+
                 }
 
 
             }
         };
+    }
+
+
+    private String getIpAddress() {
+        String ip=null;
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip = inetAddress.getHostAddress();
+                    }
+
+                }
+
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+
+        return ip;
     }
 
 
